730155b8bc6d970c18f20f60978e043b4b45eb61
zhou.mingzhe <mz.zhou@ybm100.com>
2018-12-11 21:56:31 +0800

    百度模糊搜索实现

--- demo/mobile_or_PC_web/index.js
+++ demo/mobile_or_PC_web/index.js
@@ -1,13 +1,36 @@
-function setUserAgent(window, userAgent) {
-  if (window.navigator.userAgent != userAgent) {
-      var userAgentProp = { get: function () { return userAgent; } };
-      try {
-          Object.defineProperty(window.navigator, 'userAgent', userAgentProp);
-      } catch (e) {
-          window.navigator = Object.create(navigator, {
-              userAgent: userAgentProp
-          });
+//百度模糊搜索提示
+$("#searchInput").on("keyup", function (e) {
+  $.ajax({
+    type: "get",
+    url: "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su",
+    data: {
+      wd: $(this).val(),
+      cb: "helloword"
+    },
+    dataType: "jsonp",
+    jsonpCallback: "helloword",
+    success: function (result) {
+      /*返回成功之后可以在开发者工具里的Console打印看一下*/
+      console.log(result);
+      /*将获取的数据整理后返回到页面*/
+      var a = result.s;
+      var list = "";
+      for (var i in a) {
+        var l = a[i];
+        list += "<li>" + l + "</li>";
       }
-  }
+      $("#searchData").html(list);
+    },
+    /*跨域失败的时候返回的数据*/
+    error: function () {
+      console.log("error");
+    }
+  });
+})
+
+function helloword(param) {
+console.log(param);
+
 }
-setUserAgent(document.querySelector('#mobile_view').contentWindow, 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25');
+
+//s
