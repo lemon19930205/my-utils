5a7e71e48efefd60a0b18ec8b33fb20e0537bb3e
zhou.mingzhe <mz.zhou@ybm100.com>
2019-01-09 18:11:02 +0800

    JQ  trigger

--- JSandJQ/jqSoundCode/index.js
+++ JSandJQ/jqSoundCode/index.js
@@ -8,3 +8,18 @@ let obj1 = {
   };
 
 let obj = $.extend(obj1, obj2);
+
+
+//trigger 事件
+$("#box").on("click", function (e) {
+  console.log('jqclick e: ', e);
+})
+
+var box_ele = document.getElementById("box");
+box_ele.onclick = function (e) {
+  console.log('jsclick e: ', e);
+}
+
+setTimeout(function (e) {
+  $("#box").trigger("click");
+}, 3000);
