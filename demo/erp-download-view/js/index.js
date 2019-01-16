4543903918bc8df618948db204601be93e35df75
zhou.mingzhe <mz.zhou@ybm100.com>
2019-01-16 18:38:54 +0800

    移动端电子文档下载H5

---
+++ demo/erp-download-view/js/index.js
@@ -0,0 +1,19 @@
+
+/* rem转换,以750的宽度为计算基数 */
+(function (doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      if (clientWidth >= 750) {
+        docEl.style.fontSize = '100px';
+      } else {
+        docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+      }
+    };
+
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
